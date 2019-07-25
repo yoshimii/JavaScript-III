dac1f720674216693bb8dda59b1494ff05e4ed6d
Emma Zarate <2327788+yoshimii@users.noreply.github.com>
2019-07-24 22:33:34 -0500

    answered question 2

--- assignments/this.js
+++ assignments/this.js
@@ -2,7 +2,7 @@
 * in your own words. explain the four principle for the "this" keyword below.
 *
 * 1. Window binding - When this is in the global scope the value of this will be the window object.
-* 2. 
+* 2. Implicit binding - Just like any other time we use a method (ex: array.indexOf()) the context of that method is what's on the left side of the dot. So this takes on the place of the object context name.
 * 3. New binding - 
 * 4. 
 *
